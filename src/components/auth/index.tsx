ac6ebfb811e980603674c2945239283fc4e53dec
Dun2901 <lamchidung373@gmail.com>
2026-05-11 13:33:47 +0700

    refactor title

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -3,48 +3,48 @@ import { useCurrentApp } from "components/context/app.context";
 import { Link, useLocation } from "react-router-dom";
 
 interface IProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const ProtectedRoute = (props: IProps) => {
-    const { isAuthenticated, user } = useCurrentApp();
-    const location = useLocation();
+  const { isAuthenticated, user } = useCurrentApp();
+  const location = useLocation();
 
-    if (isAuthenticated === false) {
-        return (
-            <Result
-                status="404"
-                title="Not Login"
-                subTitle="Bạn vui lòng đăng nhập để sử dụng tính năng này."
-                extra={<Button type="primary">
-                    <Link to={"/login"}>Đăng nhập</Link>
-                </Button>}
-            />
-        );
-    };
-
-    const isAdminRoute = location.pathname.includes("admin");
-    if (isAuthenticated === true && isAdminRoute === true) {
-        const role = user?.role;
-        if (role === "USER") {
-            return (
-                <Result
-                    status="403"
-                    title="403"
-                    subTitle="Sorry, you are not authorized to access this page."
-                    extra={<Button type="primary">
-                        <Link to={"/"}>Back Home</Link>
-                    </Button>}
-                />
-            );
-        };
-    };
-
+  if (isAuthenticated === false) {
     return (
-        <>
-            {props.children}
-        </>
-    )
-}
+      <Result
+        status="404"
+        title="Not Login"
+        subTitle="Trang bạn tìm kiếm không tồn tại."
+        extra={
+          <Button type="primary">
+            <Link to={"/login"}>Đăng nhập</Link>
+          </Button>
+        }
+      />
+    );
+  }
+
+  const isAdminRoute = location.pathname.includes("admin");
+  if (isAuthenticated === true && isAdminRoute === true) {
+    const role = user?.role;
+    if (role === "USER") {
+      return (
+        <Result
+          status="403"
+          title="403"
+          subTitle="Bạn không có quyền truy cập trang này."
+          extra={
+            <Button type="primary">
+              <Link to={"/"}>Back Home</Link>
+            </Button>
+          }
+        />
+      );
+    }
+  }
+
+  return <>{props.children}</>;
+};
 
 export default ProtectedRoute;
